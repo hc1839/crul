0b49ae7cf565f5639cbcef843da09e48fbb2699a
hc1839 <>
2020-03-05 16:15:00 +0000

    Define an extension function for renumbering Tripos substructures.

---
+++ src/main/kotlin/chemistry/species/format/mol2/IdentifierRenumbering.kt
@@ -0,0 +1,94 @@
+/*
+ *  Copyright Han Chen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+@file:JvmName("IdentifierRenumbering")
+@file:JvmMultifileClass
+
+package crul.chemistry.species.format.mol2
+
+import crul.chemistry.species.Supermolecule
+
+/**
+ *  Renumbers non-`null` [TriposAtom.substId], and then renumbers
+ *  [TriposAtom.atomtId] grouped by substructure.
+ *
+ *  `null` [TriposAtom.substId] are not assigned a substructure identifier but
+ *  are collectively treated as if it is one substructure during
+ *  atom-identifier renumbering.
+ *
+ *  [TriposAtom.substName] is ignored.
+ *
+ *  @param substIdStart
+ *      Index to start the renumbering of substructures.
+ *
+ *  @param atomIdStart
+ *      Index to start the renumbering of atoms. The renumbering of atoms is
+ *      such that the resulting atom identifiers are consecutive within each
+ *      substructure but do not restart between substructures.
+ *
+ *  @return
+ *      Supermolecule with substructure and atom identifiers renumbered.
+ */
+fun Supermolecule<TriposAtom>.renumberedSubstructures(
+    substIdStart: Int,
+    atomIdStart: Int
+): Supermolecule<TriposAtom>
+{
+    val oldToNewSubstIds = mutableMapOf<Int, Int>()
+    var newSubstId = substIdStart
+
+    // Renumber substructures only.
+    val renumberedSubstSupermol = map { atom ->
+        if (atom.substId != null) {
+            if (!oldToNewSubstIds.containsKey(atom.substId)) {
+                oldToNewSubstIds[atom.substId] = newSubstId++
+            }
+
+            atom.copy(
+                substId = oldToNewSubstIds[atom.substId]!!
+            )
+        } else {
+            atom
+        }
+    }
+
+    // Placeholder for the supermolecule with substructures and atoms
+    // renumbered.
+    var renumberedSupermol = renumberedSubstSupermol
+    var newAtomId = atomIdStart
+
+    // Renumber atoms for non-null substructures.
+    for (substId in (1 until newSubstId)) {
+        renumberedSupermol = renumberedSupermol.map { atom ->
+            if (atom.substId == substId) {
+                atom.copy(atomId = newAtomId++)
+            } else {
+                atom
+            }
+        }
+    }
+
+    // Renumber atoms for null substructures.
+    renumberedSupermol = renumberedSupermol.map { atom ->
+        if (atom.substId == null) {
+            atom.copy(atomId = newAtomId++)
+        } else {
+            atom
+        }
+    }
+
+    return renumberedSupermol
+}
